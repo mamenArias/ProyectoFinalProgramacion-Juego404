23f75c2aa5776e41deeccbd37fc0a68f4a5f4f42
Mª Carmen Arias <mcariasdeharo@gmail.com>
2021-05-26 20:56:53 +0200

    Retoques
    Se ha borrado la clase Inventario ya que en un principio no se va a utilizar. Se ha conectado el JList con la base de datos para que podamos seleccionar al personaje con el que queremos continuar la partida.

--- src/main/java/interfaces/PantallaCargarPersonaje.java
+++ src/main/java/interfaces/PantallaCargarPersonaje.java
@@ -33,14 +33,13 @@ import javax.swing.JScrollPane;
 public class PantallaCargarPersonaje extends JPanel{
 
 	private Ventana ventana;
-	private JTextField campoNombrePersonaje;
 	
 	public PantallaCargarPersonaje(Ventana v) {
 		setBackground(new Color(0, 0, 0));
 		this.ventana = v;
 		setLayout(null);
 		
-		JLabel labelNombre = new JLabel("Introduce el nombre de tu personaje:");
+		JLabel labelNombre = new JLabel("Selecciona tu personaje:");
 		labelNombre.setFont(new Font("MS UI Gothic", Font.PLAIN, 24));
 		labelNombre.setBackground(new Color(0, 0, 0));
 		labelNombre.setForeground(new Color(255, 255, 255));
@@ -48,26 +47,73 @@ public class PantallaCargarPersonaje extends JPanel{
 		labelNombre.setBounds(199, 46, 382, 77);
 		add(labelNombre);
 		
-		campoNombrePersonaje = new JTextField();
-		campoNombrePersonaje.setFont(new Font("MS UI Gothic", Font.PLAIN, 22));
-		campoNombrePersonaje.setHorizontalAlignment(SwingConstants.CENTER);
-		campoNombrePersonaje.setForeground(new Color(255, 0, 102));
-		campoNombrePersonaje.setBackground(new Color(0, 0, 0));
-		campoNombrePersonaje.setBounds(199, 133, 382, 46);
-		add(campoNombrePersonaje);
-		campoNombrePersonaje.setColumns(10);
+		JScrollPane scrollPersonajes = new JScrollPane();
+		scrollPersonajes.setBackground(new Color(255, 255, 255));
+		scrollPersonajes.setSize(382, 365);
+		scrollPersonajes.setLocation(199, 133);
+		add(scrollPersonajes);
 		
-		JButton botonBuscar = new JButton("Buscar");
-		botonBuscar.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		botonBuscar.setForeground(new Color(255, 255, 255));
-		botonBuscar.setFont(new Font("MS UI Gothic", Font.BOLD, 24));
-		botonBuscar.setBorderPainted(false);
-		botonBuscar.setBackground(new Color(255, 0, 102));
-		botonBuscar.addMouseListener(new MouseAdapter() {
+		JList listaPersonajes = new JList();
+		listaPersonajes.setSelectionForeground(new Color(0, 0, 0));
+		listaPersonajes.setSelectionBackground(new Color(255, 102, 153));
+		listaPersonajes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listaPersonajes.setToolTipText("");
+		listaPersonajes.setBorder(new LineBorder(new Color(255, 255, 255)));
+		
+		ArrayList<String> personajesRegistrados = new ArrayList<String>();
+		Connection c;
+		try {
+			c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/proyectoprogramacion", "root",
+					"fire_emblem3.");
+			Statement smt = c.createStatement();
+			
+			//Hacemos la consulta con select buscando los nombres de todos los protagonistas:
+			ResultSet nombresPersonaje = smt.executeQuery("select nombre from protagonista;");
+			
+			//Metemos todos los nombres en el arrayList:
+			while(nombresPersonaje.next()) {
+
+				String nombre = nombresPersonaje.getString("nombre");
+			
+				personajesRegistrados.add(nombre);
+			}
+			
+			listaPersonajes.setModel(new AbstractListModel() {
+
+				public int getSize() {
+					return personajesRegistrados.size(); // tamaño del array list
+				}
+
+				public Object getElementAt(int index) {
+					return personajesRegistrados.get(index); // obtiene los valores del array list
+				}
+			});
+			
+			smt.close();
+			c.close();
+		} catch (SQLException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+
+		listaPersonajes.setFont(new Font("MS UI Gothic", Font.PLAIN, 18));
+		listaPersonajes.setForeground(new Color(255, 255, 255));
+		listaPersonajes.setBackground(new Color(0, 0, 0));
+		listaPersonajes.setBounds(199, 328, 397, 244);
+		scrollPersonajes.setViewportView(listaPersonajes);
+		//add(listaPersonajes);
+		
+		JButton botonCargar = new JButton("Cargar");
+		botonCargar.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		botonCargar.setForeground(new Color(255, 255, 255));
+		botonCargar.setFont(new Font("MS UI Gothic", Font.BOLD, 24));
+		botonCargar.setBorderPainted(false);
+		botonCargar.setBackground(new Color(255, 0, 102));
+		botonCargar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
-				String personaje = campoNombrePersonaje.getText();
+				//Cogemos de la lista de personajes que hemos cargado el nombre seleccionado para hacer la consulta en la base de datos y cargar la partida
+				String personaje = (String)listaPersonajes.getSelectedValue();
 				
 				Connection c;
 				try {
@@ -117,75 +163,22 @@ public class PantallaCargarPersonaje extends JPanel{
 			}
 			@Override
 			public void mouseEntered(MouseEvent e) {
-				botonBuscar.setForeground(new Color(0,0,0));
-				botonBuscar.setBackground(new Color(255,102,255));
+				botonCargar.setForeground(new Color(0,0,0));
+				botonCargar.setBackground(new Color(255,102,255));
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
-				botonBuscar.setForeground(new Color(255, 255, 255));
-				botonBuscar.setBackground(new Color(255, 0, 112));
+				botonCargar.setForeground(new Color(255, 255, 255));
+				botonCargar.setBackground(new Color(255, 0, 112));
 			}
 		});
-		botonBuscar.setBounds(319, 204, 140, 46);
-		add(botonBuscar);
-		
-		JScrollPane scrollPersonajes = new JScrollPane();
-		scrollPersonajes.setSize(390, 240);
-		scrollPersonajes.setLocation(200, 332);
-		add(scrollPersonajes);
-		
-		JList listaPersonajes = new JList();
-		listaPersonajes.setToolTipText("");
-		listaPersonajes.setBorder(new LineBorder(new Color(255, 255, 255)));
-		
-		ArrayList<String> personajesRegistrados = new ArrayList<String>();
-		Connection c;
-		try {
-			c = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/proyectoprogramacion", "root",
-					"fire_emblem3.");
-			Statement smt = c.createStatement();
-			
-			//Hacemos la consulta con select buscando los nombres de todos los protagonistas:
-			ResultSet nombresPersonaje = smt.executeQuery("select nombre from protagonista;");
-			
-			//Metemos todos los nombres en el arrayList:
-			while(nombresPersonaje.next()) {
-
-				String nombre = nombresPersonaje.getString("nombre");
-			
-				personajesRegistrados.add(nombre);
-			}
-			
-			listaPersonajes.setModel(new AbstractListModel() {
-
-				public int getSize() {
-					return personajesRegistrados.size(); // tamaño del array list
-				}
-
-				public Object getElementAt(int index) {
-					return personajesRegistrados.get(index); // obtiene los valores del array list
-				}
-			});
-			
-			smt.close();
-			c.close();
-		} catch (SQLException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
-
-		listaPersonajes.setFont(new Font("MS UI Gothic", Font.PLAIN, 18));
-		listaPersonajes.setForeground(new Color(255, 255, 255));
-		listaPersonajes.setBackground(new Color(0, 0, 0));
-		listaPersonajes.setBounds(199, 328, 397, 244);
-		scrollPersonajes.setViewportView(listaPersonajes);
-		//add(listaPersonajes);
+		botonCargar.setBounds(327, 536, 140, 46);
+		add(botonCargar);
 		
 		JButton botonVolver = new JButton("Volver");
 		botonVolver.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				campoNombrePersonaje.setText("");
 				ventana.volverAInicio();
 			}
 			@Override
